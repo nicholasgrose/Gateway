4f774fea5796c7ed9a1b3fb075a5009718524a83
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2021-12-20 18:49:49 -0500

    Minor refactorings

--- src/main/kotlin/com/rose/gateway/minecraft/EventListeners.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/EventListeners.kt
@@ -1,16 +1,13 @@
 package com.rose.gateway.minecraft
 
 import com.rose.gateway.GatewayPlugin
-import com.rose.gateway.bot.extensions.chat.ChatExtension
 import com.rose.gateway.minecraft.chat.ChatListener
 import com.rose.gateway.minecraft.users.UserCount
 import org.bukkit.Server
 
 class EventListeners(val plugin: GatewayPlugin) {
     fun registerListeners(server: Server) {
-        if (plugin.configuration.notLoaded() || ChatExtension.isEnabled(plugin)) {
-            server.pluginManager.registerEvents(ChatListener(plugin), plugin)
-        }
+        server.pluginManager.registerEvents(ChatListener(plugin), plugin)
         server.pluginManager.registerEvents(UserCount(plugin), plugin)
     }
 }
