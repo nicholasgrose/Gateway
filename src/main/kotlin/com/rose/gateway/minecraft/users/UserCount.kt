028f239073fa663847cbf1c6163a76facb4da948
Nicholas Rose <nicholasgr44@gmail.com>
2022-05-21 01:44:43 -0400

    Additional update to presence on player leave

--- src/main/kotlin/com/rose/gateway/minecraft/users/UserCount.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/users/UserCount.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.runBlocking
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -15,6 +16,14 @@ class UserCount : Listener, KoinComponent {
     @EventHandler
     @Suppress("UNUSED_PARAMETER")
     fun onJoin(event: PlayerJoinEvent) {
+        runBlocking {
+            bot.presence.updatePresencePlayerCount()
+        }
+    }
+
+    @EventHandler
+    @Suppress("UNUSED_PARAMETER")
+    fun onLeave(event: PlayerQuitEvent) {
         runBlocking {
             bot.presence.updatePresencePlayerCount()
         }
