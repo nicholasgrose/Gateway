6be10afe616dcf258ec2ee25973c3457d2e5d9ee
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-08-09 00:17:45 -0400

    Documented the text processor and its token processor

--- src/main/kotlin/com/rose/gateway/shared/processing/TextProcessor.kt
+++ src/main/kotlin/com/rose/gateway/shared/processing/TextProcessor.kt
@@ -4,24 +4,55 @@ import guru.zoroark.lixy.LixyToken
 import guru.zoroark.lixy.lixy
 import guru.zoroark.lixy.matchers.matches
 
-class TextProcessor<T, A>(private val processors: List<TokenProcessor<T, A>>) {
+/**
+ * A processor that can take a piece of text, parse it, and map the parsed result into the [ResultType].
+ *
+ * @param ResultType The type to map each parsed token into.
+ * @param AdditionalDataType The additional data that should be provided to each token processing operation.
+ * @constructor Creates a TextProcessor defined by the provided [TokenProcessor]s.
+ *
+ * @param processors The list of [TokenProcessor]s to use for defining and processing tokens.
+ *
+ * @see TokenProcessor
+ */
+class TextProcessor<ResultType, AdditionalDataType>(processors: List<TokenProcessor<ResultType, AdditionalDataType>>) {
     private val tokenProcessorMap = processors.associateBy { it.tokenType() }
-
     private val lexer = lixy {
         state {
+            // This for-loop defines the regex of each processor as matching to that same processor's token type.
             for (processor in processors) {
                 matches(processor.regexPattern()) isToken processor.tokenType()
             }
         }
     }
 
-    suspend fun parseText(text: String, additionalData: A): List<T> {
+    /**
+     * Breaks a string of text into a list of [LixyToken]s to be mapped through their respective [TokenProcessor].
+     *
+     * @param text The text to parse and process.
+     * @param additionalData The additional data to be used for token processing.
+     * @return The parsed and processed text.
+     *
+     * @see TokenProcessor
+     * @see processorFor
+     */
+    suspend fun parseText(text: String, additionalData: AdditionalDataType): List<ResultType> {
         val tokens = lexer.tokenize(text)
 
         return tokens.map { token -> processorFor(token).process(token, additionalData) }
     }
 
-    private fun processorFor(token: LixyToken): TokenProcessor<T, A> {
+    /**
+     * Finds the [TokenProcessor] that handles a [LixyToken]'s type.
+     *
+     * @param token The token to find the processor for.
+     * @return The processor that can handle the token's type.
+     *
+     * @see LixyToken
+     * @see guru.zoroark.lixy.LixyTokenType
+     * @see TokenProcessor
+     */
+    private fun processorFor(token: LixyToken): TokenProcessor<ResultType, AdditionalDataType> {
         return tokenProcessorMap[token.tokenType]!!
     }
 }
--- src/main/kotlin/com/rose/gateway/shared/processing/TokenProcessor.kt
+++ src/main/kotlin/com/rose/gateway/shared/processing/TokenProcessor.kt
@@ -4,10 +4,45 @@ import guru.zoroark.lixy.LixyToken
 import guru.zoroark.lixy.LixyTokenType
 import org.intellij.lang.annotations.Language
 
-interface TokenProcessor<T, A> {
+/**
+ * Defines and processes an individual [LixyToken], giving a some result after processing it.
+ *
+ * @param ResultType The type of the result that is returned after processing a token.
+ * @param AdditionalDataType The type that represents any additional data that will be needed for processing.
+ * @constructor Creates a TokenProcessor.
+ *
+ * @see LixyToken
+ * @see LixyTokenType
+ */
+interface TokenProcessor<ResultType, AdditionalDataType> {
+    /**
+     * Gives the type of the token that this token processor defines.
+     *
+     * @return The [LixyTokenType] defined.
+     *
+     * @see LixyTokenType
+     * @see regexPattern
+     */
     fun tokenType(): LixyTokenType
 
+    /**
+     * Gives the regular expression that the defined [LixyTokenType] should be represented by in the text.
+     *
+     * @return The regular expression in the form of a string.
+     *
+     * @see tokenType
+     */
     @Language("RegExp")
     fun regexPattern(): String
-    suspend fun process(token: LixyToken, additionalData: A): T
+
+    /**
+     * Processes a [LixyToken] in conjunction with data from the [AdditionalDataType] to get a result.
+     *
+     * @param token The token to process.
+     * @param additionalData The additional data used for processing.
+     * @return The result of processing.
+     *
+     * @see LixyToken
+     */
+    suspend fun process(token: LixyToken, additionalData: AdditionalDataType): ResultType
 }
