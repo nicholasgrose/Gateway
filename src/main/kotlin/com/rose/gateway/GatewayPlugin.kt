33d2b6579c7da1b773ea180c0317681b8aa5fdb7
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-08-08 21:53:57 -0400

    Added documentation to GatewayPlugin.kt

--- src/main/kotlin/com/rose/gateway/GatewayPlugin.kt
+++ src/main/kotlin/com/rose/gateway/GatewayPlugin.kt
@@ -15,8 +15,34 @@ import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
+/**
+ * The base class and entry point for the Gateway plugin.
+ *
+ * @constructor Creates Gateway plugin. Should not be called except when the plugin is initially loaded by the server.
+ */
 class GatewayPlugin : JavaPlugin(), KoinComponent {
-    init {
+    val startTime = Clock.System.now()
+    val loader = classLoader
+    private val bot: DiscordBot by inject()
+
+    override fun onEnable() {
+        Logger.info("Starting Gateway!")
+        initializeKoin()
+
+        runBlocking {
+            bot.start()
+        }
+
+        EventListeners.registerListeners()
+        CommandRegistry.registerCommands()
+
+        Logger.info("Gateway started!")
+    }
+
+    /**
+     * Initializes Koin singletons for use throughout the Gateway plugin.
+     */
+    private fun initializeKoin() {
         startKoin {
             modules(
                 module {
@@ -28,24 +54,6 @@ class GatewayPlugin : JavaPlugin(), KoinComponent {
                 }
             )
         }
-    }
-
-    val bot: DiscordBot by inject()
-
-    val startTime = Clock.System.now()
-    val loader = classLoader
-
-    override fun onEnable() {
-        Logger.info("Starting Gateway!")
-
-        runBlocking {
-            bot.start()
-        }
-
-        EventListeners.registerListeners(server)
-        CommandRegistry.registerCommands()
-
-        Logger.info("Gateway started!")
     }
 
     override fun onDisable() {
--- src/main/kotlin/com/rose/gateway/minecraft/EventListeners.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/EventListeners.kt
@@ -3,14 +3,15 @@ package com.rose.gateway.minecraft
 import com.rose.gateway.GatewayPlugin
 import com.rose.gateway.minecraft.chat.ChatListener
 import com.rose.gateway.minecraft.users.UserCount
-import org.bukkit.Server
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 object EventListeners : KoinComponent {
     val plugin: GatewayPlugin by inject()
 
-    fun registerListeners(server: Server) {
+    fun registerListeners() {
+        val server = plugin.server
+
         server.pluginManager.registerEvents(ChatListener(), plugin)
         server.pluginManager.registerEvents(UserCount(), plugin)
     }
