df49748e71e51c816f5ceba4dd3797f5b4ad4958
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-08-09 21:12:29 -0400

    Added documentation to EventListeners.kt

--- src/main/kotlin/com/rose/gateway/minecraft/EventListeners.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/EventListeners.kt
@@ -6,10 +6,16 @@ import com.rose.gateway.minecraft.users.UserCount
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
+/**
+ * Provides functionality for adding minecraft event listeners.
+ */
 object EventListeners : KoinComponent {
     val plugin: GatewayPlugin by inject()
     val server = plugin.server
 
+    /**
+     * Registers minecraft event listeners. Should only be called once.
+     */
     fun registerListeners() {
         server.pluginManager.registerEvents(ChatListener(), plugin)
         server.pluginManager.registerEvents(UserCount(), plugin)
