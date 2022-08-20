bc2f01fb5f34079232c22f022841bc85f0b68162
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-08-20 00:51:45 -0400

    Updated Kaml and added docs to the plugin extension functions

--- src/main/kotlin/com/rose/gateway/config/extensions/MinecraftConfig.kt
+++ src/main/kotlin/com/rose/gateway/config/extensions/MinecraftConfig.kt
@@ -4,18 +4,38 @@ import com.rose.gateway.config.PluginConfig
 import com.rose.gateway.minecraft.color.asTextColor
 import net.kyori.adventure.text.format.TextColor
 
+/**
+ * Gives the Minecraft primary color from the plugin config.
+ *
+ * @return The color in a form usable for Minecraft.
+ */
 fun PluginConfig.primaryColor(): TextColor {
     return config.minecraft.primaryColor.asTextColor()
 }
 
+/**
+ * Gives the Minecraft secondary color from the plugin config.
+ *
+ * @return The color in a form usable for Minecraft.
+ */
 fun PluginConfig.secondaryColor(): TextColor {
     return config.minecraft.secondaryColor.asTextColor()
 }
 
+/**
+ * Gives the Minecraft tertiary color from the plugin config.
+ *
+ * @return The color in a form usable for Minecraft.
+ */
 fun PluginConfig.tertiaryColor(): TextColor {
     return config.minecraft.tertiaryColor.asTextColor()
 }
 
+/**
+ * Gives the Minecraft warning color from the plugin config.
+ *
+ * @return The color in a form usable for Minecraft.
+ */
 fun PluginConfig.warningColor(): TextColor {
     return config.minecraft.warningColor.asTextColor()
 }
