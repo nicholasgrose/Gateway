da372a4dc3909038ce7cfd235c556064e5efb833
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-08-21 00:17:30 -0400

    Added some docs for discord

--- src/main/kotlin/com/rose/gateway/discord/bot/DiscordBotConstants.kt
+++ src/main/kotlin/com/rose/gateway/discord/bot/DiscordBotConstants.kt
@@ -8,9 +8,23 @@ import com.rose.gateway.discord.bot.extensions.whitelist.WhitelistExtension
 import dev.kord.common.entity.Permission
 import dev.kord.common.entity.Permissions
 
+/**
+ * Constants used for the Discord bot.
+ */
 object DiscordBotConstants {
+    /**
+     * The maximum number of members that should be queried when querying for members.
+     */
     const val MEMBER_QUERY_MAX = 1
+
+    /**
+     * All the Discord bot extensions that exist in the plugin.
+     */
     val BOT_EXTENSIONS = setOf(AboutExtension, ChatExtension, IpExtension, ListExtension, WhitelistExtension)
+
+    /**
+     * The permissions the bot requires to function.
+     */
     val REQUIRED_PERMISSIONS = Permissions(
         setOf(
             Permission.SendMessages,
--- src/main/kotlin/com/rose/gateway/discord/text/DiscordBoldSafe.kt
+++ src/main/kotlin/com/rose/gateway/discord/text/DiscordBoldSafe.kt
@@ -1,5 +1,10 @@
 package com.rose.gateway.discord.text
 
+/**
+ * Changes a string so that it lacks bold formatting if sent in Discord.
+ *
+ * @return The bold-safe string.
+ */
 fun String.discordBoldSafe(): String {
     return this.replace("**", "\\**")
 }
