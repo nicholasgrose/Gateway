1c4501a1d48ee0fbdc8c2c8876ea259e3160de34
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-08-20 23:40:24 -0400

    Improved surrogate docs

--- src/main/kotlin/com/rose/gateway/shared/serialization/SurrogateBasedSerializer.kt
+++ src/main/kotlin/com/rose/gateway/shared/serialization/SurrogateBasedSerializer.kt
@@ -5,59 +5,58 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 /**
- * A class that provides functions that map from [SerializedType] to [SurrogateType] and vice versa.
+ * A class that provides functions that map from [TypeToSerialize] to [SurrogateType] and vice versa.
  *
- * @param SerializedType The target type for conversion.
+ * @param TypeToSerialize The target type for conversion.
  * @param SurrogateType The surrogate type for conversion.
  * @constructor Create a surrogate converter.
  *
  * @see SurrogateBasedSerializer
  */
-interface SurrogateConverter<SerializedType, SurrogateType> {
+interface SurrogateConverter<TypeToSerialize, SurrogateType> {
     /**
-     * Maps a serialized value [SerializedType] into a surrogate [SurrogateType].
+     * Maps a serialized value [TypeToSerialize] into a surrogate [SurrogateType].
      *
      * @param base The original value of the type.
      * @return The surrogate version of the serialized value.
      */
-    fun fromBase(base: SerializedType): SurrogateType
+    fun fromBase(base: TypeToSerialize): SurrogateType
 
     /**
-     * Maps a surrogate [SurrogateType] into a serialized value [SerializedType].
+     * Maps a surrogate [SurrogateType] into a serialized value [TypeToSerialize].
      *
      * @param surrogate The surrogate version of the serialized value.
      * @return The serialized version of the surrogate value.
      */
-    fun toBase(surrogate: SurrogateType): SerializedType
+    fun toBase(surrogate: SurrogateType): TypeToSerialize
 }
 
 /**
- * A serializer that can act as a surrogate for another type.
- * That is, it can be a serializer for a type [SerializedType] that uses a secondary type [SurrogateType]
- * as an intermediary type when serializing and deserializing for [SerializedType].
+ * A serializer that takes [TypeToSerialize], converts it to [SurrogateType] and then serializes it.
+ * It also provides the reverse operation.
  *
- * @param SerializedType The type this serializer can be a serializer for.
+ * @param TypeToSerialize The type this serializer can be a serializer for.
  * @param SurrogateType The secondary, intermediate type this serializer uses in place of T.
  * @property surrogateSerializer The serializer that handles the surrogate type [SurrogateType].
- * @property converter A converter that can map between [SerializedType] and [SurrogateType].
+ * @property converter A converter that can map between [TypeToSerialize] and [SurrogateType].
  * @constructor Create a surrogate-based serializer.
  *
  * @see KSerializer
  * @see SurrogateConverter
  */
-open class SurrogateBasedSerializer<SerializedType : Any, SurrogateType : Any>(
+open class SurrogateBasedSerializer<TypeToSerialize : Any, SurrogateType : Any>(
     private val surrogateSerializer: KSerializer<SurrogateType>,
-    private val converter: SurrogateConverter<SerializedType, SurrogateType>
-) : KSerializer<SerializedType> {
+    private val converter: SurrogateConverter<TypeToSerialize, SurrogateType>
+) : KSerializer<TypeToSerialize> {
     override val descriptor = surrogateSerializer.descriptor
 
-    override fun deserialize(decoder: Decoder): SerializedType {
+    override fun deserialize(decoder: Decoder): TypeToSerialize {
         val surrogate: SurrogateType = decoder.decodeSerializableValue(surrogateSerializer)
 
         return converter.toBase(surrogate)
     }
 
-    override fun serialize(encoder: Encoder, value: SerializedType) {
+    override fun serialize(encoder: Encoder, value: TypeToSerialize) {
         val surrogate = converter.fromBase(value)
 
         encoder.encodeSerializableValue(surrogateSerializer, surrogate)
