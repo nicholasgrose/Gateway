9810780ee2f133e16d75ef99ab01d368e0cc0d45
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-08-24 12:17:49 -0400

    Added docs for UserCount

--- src/main/kotlin/com/rose/gateway/minecraft/users/UserCount.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/users/UserCount.kt
@@ -2,33 +2,58 @@ package com.rose.gateway.minecraft.users
 
 import com.destroystokyo.paper.event.player.PlayerConnectionCloseEvent
 import com.rose.gateway.discord.bot.presence.BotPresence
-import kotlinx.coroutines.runBlocking
+import com.rose.gateway.shared.concurrency.PluginCoroutineScope
+import kotlinx.coroutines.launch
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class UserCount : Listener {
+/**
+ * Handler for events that update the Discord bot's user count.
+ *
+ * @constructor Create a user count listener.
+ */
+class UserCount : Listener, KoinComponent {
+    private val pluginCoroutineScope: PluginCoroutineScope by inject()
+
+    /**
+     * Updates the count when a player has joined the server.
+     *
+     * @param event The player-join event.
+     */
     @EventHandler
     @Suppress("UNUSED_PARAMETER")
     fun onJoin(event: PlayerJoinEvent) {
-        runBlocking {
+        pluginCoroutineScope.launch {
             BotPresence.updatePresencePlayerCount()
         }
     }
 
+    /**
+     * Updates the count when a player has quit the server.
+     *
+     * @param event The player-quit event.
+     */
     @EventHandler
     @Suppress("UNUSED_PARAMETER")
     fun onLeave(event: PlayerQuitEvent) {
-        runBlocking {
+        pluginCoroutineScope.launch {
             BotPresence.updatePresencePlayerCount()
         }
     }
 
+    /**
+     * Updates the count when a player's connection has closed on the server.
+     *
+     * @param event The "player connection close" event.
+     */
     @EventHandler
     @Suppress("UNUSED_PARAMETER")
     fun onLeave(event: PlayerConnectionCloseEvent) {
-        runBlocking {
+        pluginCoroutineScope.launch {
             BotPresence.updatePresencePlayerCount()
         }
     }
