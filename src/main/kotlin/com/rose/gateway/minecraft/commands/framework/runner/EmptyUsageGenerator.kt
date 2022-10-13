3008e7428aabf0a5d6a7c7cd3eac0ce4838cee04
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-10-13 00:14:03 -0500

    Minor refactoring and more docs for framework

--- src/main/kotlin/com/rose/gateway/minecraft/commands/framework/runner/EmptyUsageGenerator.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/commands/framework/runner/EmptyUsageGenerator.kt
@@ -1,11 +1,13 @@
 package com.rose.gateway.minecraft.commands.framework.runner
 
-fun <
-    T,
-    A : CommandArgs<A>,
-    P : ArgParser<T, A, P>
-    > emptyUsageGenerator(): (A, P) -> List<String> {
-    return { _, _ ->
-        listOf()
-    }
+/**
+ * Provides a usage generator that returns no usages
+ *
+ * @param T The type of the argument's value
+ * @param A The type of the args the argument is a part of
+ * @param P The type of the parser for the argument the result provides usages for
+ * @return A usage generator that always returns an empty set of usages
+ */
+fun <T, A : CommandArgs<A>, P : ArgParser<T, A, P>> emptyUsageGenerator(): (A, P) -> List<String> = { _, _ ->
+    listOf()
 }
--- src/main/kotlin/com/rose/gateway/minecraft/commands/framework/runner/NoArgs.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/commands/framework/runner/NoArgs.kt
@@ -1,3 +1,8 @@
 package com.rose.gateway.minecraft.commands.framework.runner
 
+/**
+ * An empty set of arguments
+ *
+ * @constructor Create no args
+ */
 class NoArgs : CommandArgs<NoArgs>()
