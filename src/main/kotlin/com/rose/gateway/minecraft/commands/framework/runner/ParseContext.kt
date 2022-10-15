9dc592916c158c4be3ec1d64f1f8c1fecebc8278
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-10-15 01:04:50 -0500

    Added ParseContext docs

--- src/main/kotlin/com/rose/gateway/minecraft/commands/framework/runner/ParseContext.kt
+++ src/main/kotlin/com/rose/gateway/minecraft/commands/framework/runner/ParseContext.kt
@@ -1,5 +1,13 @@
 package com.rose.gateway.minecraft.commands.framework.runner
 
+/**
+ * The context surrounding parsing a value
+ *
+ * @param A The type of the args in this context
+ * @property arguments The type of the args in this context
+ * @property currentIndex The next index to use from the raw args
+ * @constructor Create a parse context
+ */
 data class ParseContext<A : CommandArgs<A>>(
     val arguments: A,
     val currentIndex: Int
