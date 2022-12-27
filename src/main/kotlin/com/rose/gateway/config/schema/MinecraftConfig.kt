61eff23984f4b8ae8268286be55f9541ab3dc73a
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2022-12-26 23:48:16 -0500

    Improved descriptions for colors

--- src/main/kotlin/com/rose/gateway/config/schema/MinecraftConfig.kt
+++ src/main/kotlin/com/rose/gateway/config/schema/MinecraftConfig.kt
@@ -7,20 +7,20 @@ import kotlinx.serialization.Serializable
 /**
  * Config options for Minecraft
  *
- * @property primaryColor The color used for labels and Discord mentions in-game
- * @property secondaryColor The color used for differentiated text elements and names of Discord users in-game
- * @property tertiaryColor The color used for labelling config paths
- * @property warningColor The color used for marking problems and config items that can be null
+ * @property primaryColor Used to indicate success, color headers, and mark Discord mentions in-game
+ * @property secondaryColor Used to indicate information, differentiate text, and mark names of Discord users in-game
+ * @property tertiaryColor Used to indicate configuration paths and mark less important info
+ * @property warningColor Used to indicate errors/warnings and mark configurations that can be null
  * @constructor Create empty Minecraft config
  */
 @Serializable
 data class MinecraftConfig(
-    @ConfigItem("Used for labels and Discord mentions in-game.")
+    @ConfigItem("Used to indicate success, color headers, and mark Discord mentions in-game")
     var primaryColor: String,
-    @ConfigItem("Used for differentiated text elements and names of Discord users in-game.")
+    @ConfigItem("Used to indicate information, differentiate text, and mark names of Discord users in-game")
     var secondaryColor: String,
-    @ConfigItem("Used for labelling config paths.")
+    @ConfigItem("Used to indicate configuration paths and mark less important info")
     var tertiaryColor: String,
-    @ConfigItem("Used for marking problems and config items that can be null.")
+    @ConfigItem("Used to indicate errors/warnings and mark configurations that can be null")
     var warningColor: String
 ) : ConfigObject
