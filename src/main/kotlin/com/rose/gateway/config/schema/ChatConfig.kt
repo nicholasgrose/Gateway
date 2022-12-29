ccb8d990fc3b38fe984b76970bc2f69298d35f69
Epicjii <jacobliggett2000@gmail.com>
2022-12-28 23:32:59 -0500

    Fixed changes

--- src/main/kotlin/com/rose/gateway/config/schema/ChatConfig.kt
+++ src/main/kotlin/com/rose/gateway/config/schema/ChatConfig.kt
@@ -10,22 +10,22 @@ import kotlinx.serialization.Serializable
 /**
  * Config options for the "chat extension"
  *
+ * @property showRoleColor Whether Discord role colors are shown
  * @constructor Creates a "chat config" with the provided data
  *
  * @param enabled Whether the extension is enabled
- * @property showRoleColor Whether discord role colors are shown
  */
 @Serializable(with = ChatConfigSerializer::class)
 class ChatConfig(
     enabled: Boolean,
-    @ConfigItem("Whether discord role colors are shown in minecraft chat") val showRoleColor: Boolean
+    @ConfigItem("Whether Discord role colors are shown in Minecraft chat") val showRoleColor: Boolean
 ) : CommonExtensionConfig(enabled, ChatExtension.extensionName())
 
 /**
  * Surrogate for serialization of [ChatConfig]
  *
  * @property enabled Whether the extension is enabled
- * @property showRoleColor Whether discord role colors are shown
+ * @property showRoleColor Whether Discord role colors are shown
  * @constructor Create a "chat config" surrogate with the provided data
  *
  * @see ChatConfig
