4960ea6dd116512b47f1b829e71b3ff7f7d95c69
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2024-12-05 22:27:10 -0600

    Update to gradle 8.11

--- buildSrc/src/main/kotlin/gateway/conventions/expand-versions.gradle.kts
+++ buildSrc/src/main/kotlin/gateway/conventions/expand-versions.gradle.kts
@@ -1,7 +1,5 @@
 package gateway.conventions
 
-import gradle.kotlin.dsl.accessors._18b46bc62d2d24efa2e4fdac1d739e7a.processResources
-
 /**
  * Extension for configuring the expand versions convention plugin
  *
@@ -16,7 +14,7 @@ val extension = project.extensions.create<ExpandVersionsPluginExtension>("files"
 extension.filePattern.convention("")
 
 tasks {
-    processResources {
+    getByName<ProcessResources>("processResources") {
         filesMatching(extension.filePattern.get()) {
             expand(
                 "version" to version,
